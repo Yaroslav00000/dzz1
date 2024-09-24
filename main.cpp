194c7cd0dad5efd8bea7059dc8e6a40d5261ea89
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-09-24 22:49:34 +0300

    +

--- main.cpp
+++ main.cpp
@@ -1,1 +1,29 @@
-#in
+#include <iostream>
+#include "Window.h"
+
+using namespace std;
+
+class House
+{
+private:
+	int m_rooms;
+	int m_floors;
+	int m_area;
+	st m_address;
+public:
+
+};
+class Room
+{
+private:
+
+public:
+
+};
+class Human
+{
+private:
+	string first_name;
+	int age;
+	
+};
